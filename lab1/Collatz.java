44a7f054db8c4edc04ab643bf8522724ddd629b6
chu123122 <yorushika123122@163.com>
2024-09-05 22:28:54 +0800

    finish the lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,10 +2,25 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
+
+    /** Buggy implementation of nextNumber! */
+    public static int nextNumber(int n) {
+        if(n ==1 ) return  1;
+        if(n%2==0){
+            return n/2;
+        }else {
+            return 3*n+1;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
-
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
 
