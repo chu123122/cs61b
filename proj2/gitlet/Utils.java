f0a105a5674ad20e37cd726bb0f254df2dd97896
chu123122 <yorushika123122@163.com>
2024-10-11 14:05:36 +0800

    try to find the bug of status and fix it

--- proj2/gitlet/Utils.java
+++ proj2/gitlet/Utils.java
@@ -187,12 +187,12 @@ class Utils {
      * Filter out all but plain files.
      */
     private static final FilenameFilter PLAIN_FILES =
-            new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String name) {
-                    return new File(dir, name).isFile();
-                }
-            };
+        new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return new File(dir, name).isFile();
+            }
+        };
 
     /**
      * Returns a list of the names of all plain files in the directory DIR, in
